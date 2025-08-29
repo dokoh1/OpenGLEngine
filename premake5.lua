2d278720bb0cb46dcc5484d9cfbbdd3f0f74dd21
OH DOKYUNG <146950802+dokoh1@users.noreply.github.com>
2025-08-29 13:42:05 +0900

    premake를 이용한 build 설정 구성

---
+++ premake5.lua
@@ -0,0 +1,112 @@
+workspace "Hazel"
+    architecture "x64"
+    
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Hazel"
+    location "Hazel"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/**.h",
+        "%{prj.name}/**.cpp",
+    }
+    removefiles { "Hazel/vendor/**.cpp", "Hazel/vendor/**.c" }
+
+    includedirs
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        buildoptions { "/utf-8" }
+        cppdialect "C++17"
+        staticruntime "On"
+
+        systemversion "latest"
+        
+        defines
+        {
+            "HZ_PLATFORM_WINDOWS",
+            "HZ_BUILD_DLL"
+        }
+
+        postbuildcommands
+        {
+            ("{COPYFILE} %{cfg.buildtarget.relpath} ../bin/"  .. outputdir .. "/Sandbox")
+
+        }
+    
+    filter "configurations:Debug"
+        defines "HZ_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "HZ_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "HZ_DIST"
+        optimize "On"
+
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/**.h",
+        "%{prj.name}/**.cpp",
+    }
+
+    includedirs
+    {
+        "$(SolutionDir)Hazel/vendor/spdlog/include",
+        "$(SolutionDir)Hazel"
+    }
+
+
+    links
+    {
+        "Hazel"
+    }
+
+    filter "system:windows"
+        buildoptions { "/utf-8" }
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+        
+        defines
+        {
+            "HZ_PLATFORM_WINDOWS",
+        }
+
+    filter "configurations:Debug"
+        defines "HZ_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "HZ_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "HZ_DIST"
+        optimize "On"
